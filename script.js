6790e009d76bc9fdb63b5df47af73ad9267812d4
Tanawat.Gut <63237781+KhaiTomGut2006@users.noreply.github.com>
2025-10-08 14:49:19 +0700

    .env

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-	import { initializeApp } from "firebase/app";
-			import { getAnalytics } from "firebase/analytics";
-			const firebaseConfig = {
-				apiKey: "AIzaSyDCjt8DfkKCsjc73Oaay851FYu8pG1-3TY",
-				authDomain: "egoke-7dae5.firebaseapp.com",
-				projectId: "egoke-7dae5",
-				storageBucket: "egoke-7dae5.firebasestorage.app",
-				messagingSenderId: "910235640821",
-				appId: "1:910235640821:web:cc5163a4eee3e8dffc76bc",
-				measurementId: "G-10MPJ3TPEB"
-			};
-			const app = initializeApp(firebaseConfig);
-			const analytics = getAnalytics(app);
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+require('dotenv').config();
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: "egoke-7dae5.firebasestorage.app",
+  messagingSenderId: "910235640821",
+  appId: "1:910235640821:web:cc5163a4eee3e8dffc76bc",
+  measurementId: "G-10MPJ3TPEB"
+};
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
